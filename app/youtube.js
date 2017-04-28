e264052e5751b033f6a3a7879127f16f456acda3
wasnot <wasnot.apps@gmail.com>
2017-04-28 18:08:03 +0900

    apply text new literal

--- app/youtube.js
+++ app/youtube.js
@@ -15,14 +15,14 @@ class YoutubeVideo {
 function getYoutubeInfo(video_url, callback) {
   const yt_video_id = YoutubeInfo.getYoutubeIdByUrl(video_url);
   if (yt_video_id) {
-    const key = $.getJSON('https://www.googleapis.com/youtube/v3/videos?id=' + yt_video_id + '&part=snippet,contentDetails&key=' + config.yt_key + '&fields=items(id,snippet(title,thumbnails),contentDetails(duration))')
+    const key = $.getJSON(`https://www.googleapis.com/youtube/v3/videos?id=${yt_video_id}&part=snippet,contentDetails&key=${config.yt_key}&fields=items(id,snippet(title,thumbnails),contentDetails(duration))`)
       .done((data, status, xhr) => {
         console.log(data);
         const yt_response = data.items[0], // If you need more video informations, take a look on this response: data.data
           yt_title = yt_response.snippet.title,
           yt_duration = YoutubeInfo.formatSecondsAsTime(yt_response.contentDetails.duration),
           yt_thumb = yt_response.snippet.thumbnails.default.url,
-          yt_url = 'https://www.youtube.com/watch?v=' + yt_video_id;
+          yt_url = `https://www.youtube.com/watch?v=${yt_video_id}`;
         const video = new YoutubeVideo(yt_title, yt_url, yt_video_id, yt_thumb);
         if (typeof callback === 'function'){
           callback(true, video);
